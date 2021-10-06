997be0fb5d0edc2fa666546bf62ce81999176bab
yossil20000 <44961798+yossil20000@users.noreply.github.com>
2021-10-06 21:59:43 +0300

    Add exercise

---
+++ Lessons/Lesson14/script.js
@@ -0,0 +1,12 @@
+var x;
+var y;
+x = prompt("Get the x value");
+y = prompt("Get y value" ,"1");
+if(x > y)
+{
+    console.log("BOOM " + x * y);
+}
+else{
+    alert("try again");
+}
+alert("Answaer : " + x*y);
