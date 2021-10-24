c05973b9f1148bda520f24283932893e333b0eea
yossil20000 <44961798+yossil20000@users.noreply.github.com>
2021-10-24 07:15:16 +0300

    Exercise16

--- Lessons/Lesson16/homework_1.js
+++ Lessons/Lesson16/homework_1.js
@@ -1,11 +1,23 @@
 function login(userName,password){
+    if(userName == undefined )
+    userName = document.getElementById("login-name").value;
+    if(password == undefined)
+        password = document.getElementById("login-password").value;
+    if(userName == null || password == null)
+        {SetLoginMessage("One of the parameter is invalid");    return "";}
     if(userName.toLowerCase() == "admin" && password == "123456")
     {
+        SetLoginMessage("Logging Succeed");
         return "Logging Succeed";
     }
+    SetLoginMessage("One of the parameter is invalid");
     return "One of the parameter is invalid";
 }
+function SetLoginMessage(message)
+{
+    document.getElementById("form-login-validation").innerHTML = `<span style='color: red;'>${message}</span>`;
+}
 
-var userNameBad = login("yossi", "123456");
+/* var userNameBad = login("yossi", "123456");
 var passwordBad = login("admin","1222");
-var good = login("admin","123456");
+var good = login("admin","123456"); */
