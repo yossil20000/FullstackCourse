1015689d2bc8e9942766604c704b0b3cb8cc36ea
yossil20000 <44961798+yossil20000@users.noreply.github.com>
2021-11-01 18:03:12 +0200

    October27

---
+++ Lessons/AppendChild/Card.js
@@ -0,0 +1,18 @@
+let bodyElement = document.body;
+let cardElement = document.createElement('div');
+let imageContainer = document.createElement('div');
+let infoContainer = document.createElement('div');
+let imageElement = document.createElement('img');
+let headingElement = document.createElement('h5');
+let paragraphElement = document.createElement('p');
+let btnElement = document.createElement('a');
+
+// the class name property
+cardElement.className = "card";
+imageContainer.className ="image-container";
+console.log(imageContainer);
+infoContainer.className = "info-container";
+headingElement.className = "heading";
+paragraphElement.className = "paragraph";
+btnElement = "btn";
+// 
--- Lessons/AppendChild/child.js
+++ Lessons/AppendChild/child.js
@@ -10,3 +10,26 @@ function createMenuItem(name) {
     li.textContent = name;
     menuItem.appendChild(li); 
 }
+
+const ul = document.querySelector(".dc-overview");
+//using innerHTML
+var numVisit = 56;
+var existingListItem = document.querySelectorAll(".dc-overview > li")[2];
+//This code delete all the element insert before
+/* existingListItem.innerHTML = "<strong>I have"+ numVisit + " been modified!"; */
+//Or using ` `
+existingListItem = `<li><strong>You have visit ${numVisit} times 2</strong></li>`;
+//Addin new to exist one way
+ul.innerHTML += existingListItem;
+//more elegant and powerfull insertAdjacentHTML
+ul.insertAdjacentHTML("beforeend",`<li>i am new beforeend </li>`);
+ul.insertAdjacentHTML("afterbegin",`<li>i am new afterbegin  </li>`);
+ul.insertAdjacentHTML("beforebegin",`<li>i am new beforbegin </li>`);
+//with AdacentElement
+var newelement = document.createElement("li");
+numVisit = 58;
+existingListItem = `<li><strong>You have visit ${numVisit} times 2</strong></li>`;
+newelement.innerHTML = existingListItem;
+numVisit = 60;
+ul.insertAdjacentElement("afterend",newelement);
+ul.insertAdjacentHTML("beforebegin",existingListItem);
