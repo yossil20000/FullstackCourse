39d37a8b761730c2914a17ce1d2b94b6b0fd752b
yossil20000 <44961798+yossil20000@users.noreply.github.com>
2021-11-23 06:23:34 +0200

    NoteBoard
    Class Display to initial media for device, stil hav init bug

--- Project/NoteBoard/js/GeneralFunction.js
+++ Project/NoteBoard/js/GeneralFunction.js
@@ -1,3 +1,34 @@
+let mobileWidthQuery = getComputedStyle(document.documentElement).getPropertyValue('--mobile-width');
+var tabletWidthQuery = getComputedStyle(document.documentElement).getPropertyValue('--tablet-width');
+let deviceMobile = window.matchMedia(`(min-width:${mobileWidthQuery})`);
+let deviceTablet = window.matchMedia(`(min-width:${tabletWidthQuery})`);
+deviceMobile.addEventListener('change', handleDeviceChange);
+deviceTablet.addEventListener('change',handleDeviceChange);
+
+class Display{
+    static Mobile = new Display('Mobile');
+    static Tablet = new Display('Tablet');
+    static Desktop = new Display('Desktop');
+    name;
+    constructor(name) {this.name = name;}
+    IsMobile(){
+        
+        return this.name ===  Display.Mobile;
+    }
+    IsTablet() {
+        return this.name === Display.Tablet;
+    }
+    IsDesktop() {
+        return this.name === Display.Desktop;
+    }
+    toString(){
+        return `Display.${this.name}`;
+    }
+}
+let typeScreen = new Display(Display.Mobile);
+handleDeviceChange(deviceTablet);
+
+
 /* const eDisplay = {
     Mobile = 'Mobile',
     Tablet =  'Tablet',
@@ -8,47 +39,25 @@
     IsTablet(val) {return val === this.Tablet;},
     IsDesktop(val) {return val === this.Desktop;}
 }; */
-class Display{
-    static Mobile = new Display('Mobile');
-    static Tablet = new Display('Tablet');
-    static Desktop = new Display('Desktop');
-    name;
-    constructor() {this.name = this.Mobile;}
-    IsMobile(){
-        return this.name ==  this.Mobile;
-    }
-    IsTablet(val) {
-        return this.name == this.Tablet;
-    }
-    IsDesktop(val) {
-        return this.name === this.Desktop;
-    }
-}
-var mobileWidthQuery = getComputedStyle(document.documentElement).getPropertyValue('--mobile-width');
-var tabletWidthQuery = getComputedStyle(document.documentElement).getPropertyValue('--tablet-width');
-const deviceMobile = window.matchMedia(`(min-width:${mobileWidthQuery})`);
-const deviceTablet = window.matchMedia(`(min-width:${tabletWidthQuery})`);
-var typeScreen = new Display();
-deviceMobile.addEventListener('change', handleDeviceChange);
-deviceTablet.addListener(handleDeviceChange);
+
+
 function handleDeviceChange(e){
     console.log(e.media.includes(mobileWidthQuery));
     if(e.matches && e.media.includes(mobileWidthQuery))
     {
-        typeScreen.name = Display.Mobile;
-            console.log(`match mobile ${mobileWidthQuery} typeScreen:${typeScreen.IsMobile()}`);
+        typeScreen.name = Display.Tablet;
+            console.log(`match mobile ${mobileWidthQuery} typeScreen:${typeScreen.IsTablet()}`);
     }
-    else if(!e.matches && e.media.includes(tabletWidthQuery)){
-        typeScreen.name= Display.Tablet;
-        console.log(`match ${tabletWidthQuery} typeScreen:${typeScreen.IsTablet()}`);
-    }
-    else {
-        typeScreen.name = Display.Desktop;
+    else if(e.matches && e.media.includes(tabletWidthQuery)){
+        typeScreen.name= Display.Desktop;
         console.log(`match ${tabletWidthQuery} typeScreen:${typeScreen.IsDesktop()}`);
     }
+    else {
+        typeScreen.name = Display.Mobile;
+        console.log(`match ${mobileWidthQuery} typeScreen:${typeScreen.IsMobile()}`);
+    }
 }
-handleDeviceChange(deviceMobile);
-handleDeviceChange(deviceTablet);
+
 function setDateTime(element, date) {
   
     const dateForDateTimeInputValue = date => new Date(date.getTime() + new Date().getTimezoneOffset() * -60 * 1000).toISOString().slice(0, 19);
