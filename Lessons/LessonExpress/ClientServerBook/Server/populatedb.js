c7e84954b2ebf3ca8227d1519b7be1bf3b024f8f
YLPilot <yos.1965@gmail.com>
2022-02-18 22:37:47 +0200

    Add MongoDB connection string

--- Lessons/LessonExpress/ClientServerBook/Server/populatedb.js
+++ Lessons/LessonExpress/ClientServerBook/Server/populatedb.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
-
-console.log('This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true');
+require('dotenv').config()
+//console.log('This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true');
 
 // Get arguments passed on command line
 var userArgs = process.argv.slice(2);
@@ -18,7 +18,9 @@ var BookInstance = require('./models/bookinstance')
 
 
 var mongoose = require('mongoose');
-var mongoDB = userArgs[0];
+var mongoDB = userArgs[0] === undefined ?  process.env.MONGODB_URL : userArgs[0] ;
+//console.log(mongoDB);
+
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
